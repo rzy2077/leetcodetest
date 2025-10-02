8652d32b971d3012867e4494504660e53238d6b1
开始提交代码 <renzhiyuan@huawei.com>
2025-10-03 01:20:06 +0800

    更改

--- interview88/287.py
+++ interview88/287.py
@@ -15,14 +15,24 @@ class Solution:
         return result
     def findDuplicate(self, nums: List[int]) -> int:
         #You must solve the problem without modifying the array nums(sort array) and using only constant extra space.
-        l1 = len(nums)
-        d1 = defaultdict(int)
+        #对有序数组进行二分，有序数组为n出现的次数
         result = -1
-        for x in nums:
-            d1[x] += 1
-            if d1[x] > 1:
-                return x
-  #  12 34
+        l1 = len(nums)
+        left = 0
+        right = l1 - 1
+        while left <= right:
+            mid = (right - left)//2 + left
+            count = 0
+            for i in nums:
+                if i <= mid:
+                    count += 1
+            if count <= mid :
+                left = mid + 1
+            else:
+                right = mid - 1
+                result = mid
+
+
         return result
 if __name__ == '__main__':
         s1 = Solution()
